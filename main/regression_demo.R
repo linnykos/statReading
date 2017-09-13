89c4ee42e6ba966c87061fc626322cf4fb2d2857
klin1234@gmail.com <klin1234@gmail.com>
2017-09-13 15:25:24 -0400

    Added the estimated OLS

--- main/regression_demo.R
+++ main/regression_demo.R
@@ -1,7 +1,14 @@
+rm(list=ls())
 density_func <- function(x,y){
-  stats::dnorm(x)*stats::dnorm(y)
+  mvtnorm::dmvnorm(c(x,y), mean = c(-1,1), sigma = matrix(c(2,-1,-1,2),2,2))
 }
 
 xlim <- c(-5, 5); ylim <- c(-5, 5)
 mat <- create_prob_grid(xlim, ylim, density_func)
-plot_grid(mat)
+plot_grid(mat, asp = T)
+
+dat <- sample_from_grid(mat)
+graphics::points(dat, pch = 16)
+res <- ols(dat)
+
+plot_ols(res, xlim, lwd = 2)
--- statReading/R/regression.R
+++ statReading/R/regression.R
@@ -10,8 +10,33 @@ create_prob_grid <- function(xlim, ylim, density_func, grid_size = 200){
 plot_grid <- function(mat, type = c("contour"), alpha = 0.5, nlevels = 10, ...){
   xseq <- as.numeric(colnames(mat)); yseq <- as.numeric(rownames(mat))
 
-  image(xseq, yseq, mat, col = grDevices::heat.colors(100, alpha = alpha))
+  image(xseq, yseq, mat, col = grDevices::heat.colors(100, alpha = alpha), ...)
   contour(xseq, yseq, mat, nlevels = nlevels, add = T, ...)
 }
 
-clockwise90 <- function(mat) { t(mat[nrow(mat):1,]) }
+sample_from_grid <- function(mat, n = 50){
+  xseq <- as.numeric(colnames(mat)); yseq <- as.numeric(rownames(mat))
+  h <- nrow(mat); w <- ncol(mat)
+  vec <- as.numeric(mat); len <- length(vec)
+  idx <- sample(1:len, n, prob = vec, replace = T)
+
+  dat <- sapply(idx, function(i){
+    x <- ifelse(i %% h == 0, h, i %% h)
+    y <- floor(i/h)
+    c(xseq[x],yseq[y])
+  })
+
+  t(dat)
+}
+
+#no intercept
+ols <- function(dat){
+  x <- dat[,1]; y <- dat[,2]
+
+  (t(x)%*%x)^(-1)*t(x)%*%y
+}
+
+plot_ols <- function(res, xlim, ...){
+  y1 <- res*xlim[1]; y2 <- res*xlim[2]
+  graphics::lines(xlim, c(y1, y2), ...)
+}
