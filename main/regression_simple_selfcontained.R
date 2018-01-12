77655205499fc7eedb1b676cf4dae0f7d15529ec
klin1234@gmail.com <klin1234@gmail.com>
2018-01-12 17:34:01 -0500

    Removed dependency of mvtnorm package

--- main/regression_simple_selfcontained.R
+++ main/regression_simple_selfcontained.R
@@ -1,4 +1,9 @@
 rm(list=ls())
+
+dmvnorm <- function(x, mean, sigma){
+  as.numeric(exp(-.5*t(x-mean)%*%solve(sigma)%*%(x-mean)) / sqrt((2*pi)^length(mean)*det(sigma)))
+}
+
 #create a density over a grid
 create_prob_grid <- function(xlim, ylim, density_func, grid_size = 100){
   xseq <- seq(xlim[1], xlim[2], length.out = grid_size)
@@ -14,7 +19,7 @@ create_prob_grid <- function(xlim, ylim, density_func, grid_size = 100){
 plot_grid <- function(mat, type = c("contour"), alpha = 0.5, nlevels = 10, ...){
   xseq <- as.numeric(colnames(mat)); yseq <- as.numeric(rownames(mat))
 
-  image(xseq, yseq, mat, col = grDevices::heat.colors(100, alpha = alpha), ...)
+  image(xseq, yseq, mat, col = heat.colors(100, alpha = alpha), ...)
   contour(xseq, yseq, mat, nlevels = nlevels, add = T, drawlabels = F, ...)
 }
 
@@ -44,7 +49,7 @@ ols <- function(dat){
 #add the OLS regression line to plot
 plot_ols <- function(res, xlim, ...){
   y1 <- res*xlim[1]; y2 <- res*xlim[2]
-  graphics::lines(xlim, c(y1, y2), ...)
+  lines(xlim, c(y1, y2), ...)
 }
 
 #compute the population OLS coefficent based on the grid, minimizing expected risk
@@ -76,12 +81,12 @@ population_regression <- function(mat){
 set.seed(10)
 
 density_func <- function(x,y){
-  val1 <- mvtnorm::dmvnorm(c(x,y), mean = c(-2,2), sigma = 2*matrix(c(2,-1,-1,2),2,2))
-  val2 <- mvtnorm::dmvnorm(c(x,y), mean = c(-1.8,1.8), sigma = 2*matrix(c(2,1,1,2),2,2))
-  val3 <- mvtnorm::dmvnorm(c(x,y), mean = c(3,-3), sigma = matrix(c(3,.2,.2,3),2,2))
-  val4 <- mvtnorm::dmvnorm(c(x,y), mean = c(2,-2), sigma = matrix(c(3,-2,-2,3),2,2))
-  val5x <- stats::dnorm(x, mean = -2)
-  val5y <- stats::dnorm(x^2-3-2*y)
+  val1 <- dmvnorm(c(x,y), mean = c(-2,2), sigma = 2*matrix(c(2,-1,-1,2),2,2))
+  val2 <- dmvnorm(c(x,y), mean = c(-1.8,1.8), sigma = 2*matrix(c(2,1,1,2),2,2))
+  val3 <- dmvnorm(c(x,y), mean = c(3,-3), sigma = matrix(c(3,.2,.2,3),2,2))
+  val4 <- dmvnorm(c(x,y), mean = c(2,-2), sigma = matrix(c(3,-2,-2,3),2,2))
+  val5x <- dnorm(x, mean = -2)
+  val5y <- dnorm(x^2-3-2*y)
 
   0.4*val1 + 2*val2 + 0.3*val3 + 0.3*val4 + 0.5*val5x*val5y
 }
@@ -89,12 +94,12 @@ density_func <- function(x,y){
 xlim <- c(-5, 5); ylim <- c(-5, 5)
 mat <- create_prob_grid(xlim, ylim, density_func)
 
-grDevices::pdf("figure.pdf", 3, 3)
-graphics::par(mar = rep(0, 4))
+pdf("figure.pdf", 3, 3)
+par(mar = rep(0, 4))
 plot_grid(mat, asp = T, xlab = "", ylab = "", xaxt = "n", yaxt = "n", bty = "n")
 set.seed(5)
 dat <- sample_from_grid(mat, n = 50)
-graphics::points(dat, pch = 16, col = rgb(0,0,0,0.5))
+points(dat, pch = 16, col = rgb(0,0,0,0.5))
 res <- ols(dat)
 plot_ols(res, xlim, lwd = 4, lty = 4, col = "blue")
 
@@ -102,6 +107,6 @@ pop_ols <- population_ols(mat, betalim = c(-1,1))
 plot_ols(pop_ols, xlim, lwd = 4, lty = 4)
 
 reg <- population_regression(mat)
-graphics::lines(reg[,1], reg[,2], lwd = 4, col = 3)
-grDevices::graphics.off()
+lines(reg[,1], reg[,2], lwd = 4, col = 3)
+graphics.off()
 
